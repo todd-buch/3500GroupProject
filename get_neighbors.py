c7eb7df4aa664986c9562b04e731de665e75ae00
Todd Buch <todd.buch@uconn.edu>
2025-03-27 15:19:12 -0400

    fixed indentation

--- get_neighbors.py
+++ get_neighbors.py
@@ -1,11 +1,11 @@
-def grid_2d(end, node):
+def grid_2d(grid_size, node):
     neighbors = [
         (node[0] - 1, node[1]),
         (node[0] + 1, node[1]),
         (node[0], node[1] - 1),
         (node[0], node[1] + 1)
     ]
-    valid_neighbors = [n for n in neighbors if 0 <= n[0] < end and 0 <= n[1] < end]
+    valid_neighbors = [n for n in neighbors if 0 <= n[0] < grid_size and 0 <= n[1] < grid_size]
     return [(n, 1) for n in valid_neighbors]
 
 def weighted_graph(graph, node):
