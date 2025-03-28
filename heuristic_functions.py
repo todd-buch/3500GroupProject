e45a31fa61750e909cca0ef64ae36eb8a396de70
Fenglin Zhao <FENGLIN.ZHAO@UCONN.EDU>
2025-03-27 20:51:28 -0400

    Update heuristic_functions.py

--- heuristic_functions.py
+++ heuristic_functions.py
@@ -6,4 +6,10 @@ def manhattan_distance(node, goal):
     '''Return the Manhattan distance between the current node and the goal node. Only works for 2D grids.'''
     return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
 
-# TODO: add other heuristic functions that prof said to use
+def euclidean_distance(node, goal):
+    '''Return the Euclidean distance between the current node and the goal node.'''
+    return sqrt( (node[0] - goal[0]) ** 2 + (node[1] - goal[1]) )
+    
+def chebyshev_distance(node, goal):
+    '''Return the Chebyshev distance between the current node and the goal node.'''
+    return max( abs(node[0] - goal[0]), abs(node[1] - goal[1]) )
