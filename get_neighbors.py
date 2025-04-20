054a270b8e2438d1b09937c38d9c6637e2018d1a
dennis gega <dennis.gega@uconn.edu>
2025-04-20 13:48:02 -0400

    Removed all code related to weighted graphs

--- get_neighbors.py
+++ get_neighbors.py
@@ -7,6 +7,3 @@ def grid_2d(grid_size, node):
     ]
     valid_neighbors = [n for n in neighbors if 0 <= n[0] < grid_size and 0 <= n[1] < grid_size]
     return [(n, 1) for n in valid_neighbors]
-
-def weighted_graph(graph, node):
-    return graph.get(node, [])
