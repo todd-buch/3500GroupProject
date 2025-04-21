b82f758eb8b2df4956abfd9bd99691a8a69e044c
dennis gega <dennis.gega@uconn.edu>
2025-04-21 19:41:03 -0400

    Added docstrings to all tests and added one test to 2d

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ class TestAStarSearch(unittest.TestCase):
 
 
     def test_empty_grid_2d(self):
-        # Testing the trivial case - 0x0 2d grid
+        '''Testing the trivial case - 0x0 2d grid'''
         grid_size = 0
         start = (0, 0)
         goal = (0, 0)
@@ -48,96 +48,97 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), expected_length)
 
     def test_grid_4x4_2d(self):
-        # Testing small 2d grid (4x4)
+        '''Testing small 2d grid (4x4)'''
         grid_size = 4
         start = (0, 0)
         goal = (3, 3)
 
-        # Iterate through each heuristic for 2d grids
         for name, heuristic in heuristics_2d.items():
             with self.subTest(heuristic=name):
-                # Create graph and test for correct output path
                 path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
                 self.assertIsNotNone(path)
                 self.assertEqual(path[0], start)
                 self.assertEqual(path[-1], goal)
-                # Test for correct output path length
                 expected_length = abs(goal[0] - start[0]) + abs(goal[1] - start[1]) + 1
                 self.assertEqual(len(path), expected_length)
 
     def test_grid_1000x1000_2d(self):
-        # Testing large 2d grid (1000x1000)
+        '''Testing large 2d grid (1000x1000)'''
         grid_size = 1000
         start = (0, 0)
         goal = (99, 99)
 
-        # Iterate through each heuristic for 2d grids
         for name, heuristic in heuristics_2d.items():
             with self.subTest(heuristic=name):
-                # Create graph and test for correct output path
                 path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
                 self.assertIsNotNone(path)
                 self.assertEqual(path[0], start)
                 self.assertEqual(path[-1], goal)
-                # Test for correct output path length
                 expected_length = abs(goal[0] - start[0]) + abs(goal[1] - start[1]) + 1
                 self.assertEqual(len(path), expected_length)
 
     def test_no_possible_path_2d(self):
-        # Test when no possible path exists between start and path
+        '''Test when no possible path exists between start and path'''
         grid_size = 10
         start = (0, 0)
         goal = (10, 10) # Goal is outside of grid bounds
 
-        # Iterate through each heuristic for 2d grids
         for name, heuristic in heuristics_2d.items():
             with self.subTest(heuristic=name):
-                # Create graph and test for correct output path
                 path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
                 self.assertIsNotNone(path)
                 self.assertEqual(path, [])
-                # Test for correct output path length
                 expected_length = 0
                 self.assertEqual(len(path), expected_length)
 
     def test_start_equals_goal_2d(self):
-        # Test when start and goal nodes are the same
+        '''Test when start and goal nodes are the same'''
         grid_size = 10
         start = (7, 7)
         goal = (7, 7) # Goal is the same as the start and within grid bounds
 
-        # Iterate through each heuristic for 2d grids
         for name, heuristic in heuristics_2d.items():
             with self.subTest(heuristic=name):
-                # Create graph and test for correct output path
                 path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
                 self.assertIsNotNone(path)
                 self.assertEqual(path[0], start)
                 self.assertEqual(path[-1], goal)
-                # Test for correct output path length
                 expected_length = abs(goal[0] - start[0]) + abs(goal[1] - start[1]) + 1
                 self.assertEqual(len(path), expected_length)
 
     def test_random_grids_2d(self):
-        # Test various randomized 2d grids and inputs
+        '''Test various randomized 2d grids and inputs'''
         for _ in range(25):
             grid_size = randint(1, 100)
             start = (randint(0, grid_size-1), randint(0, grid_size-1))
             goal = (randint(0, grid_size-1), randint(0, grid_size-1))
 
-            # Iterate through each heuristic for 2d grids
             for name, heuristic in heuristics_2d.items():
                 with self.subTest(heuristic=name):
-                    # Create graph and test for correct output path
                     path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
                     self.assertIsNotNone(path)
                     self.assertEqual(path[0], start)
                     self.assertEqual(path[-1], goal)
-                    # Test for correct output path length
                     expected_length = abs(goal[0] - start[0]) + abs(goal[1] - start[1]) + 1
                     self.assertEqual(len(path), expected_length)
 
+    def test_edge_traversal_2d(self):
+        '''Test when optimal path is along grid edge'''
+        grid_size = 5
+        start = (0, 0)
+        goal = (0, 4)  # Travel along edge only
+        for name, heuristic in heuristics_2d.items():
+            with self.subTest(heuristic=name):
+                path = a_star_search(grid_size, start, goal, grid_2d, heuristic)
+                self.assertIsNotNone(path)
+                self.assertEqual(path[0], start)
+                self.assertEqual(path[-1], goal)
+                expected_length = abs(goal[0] - start[0]) + abs(goal[1] - start[1]) + 1
+                self.assertEqual(len(path), expected_length)
+
+
     def test_empty_grid_3d(self):
+        '''Test the trivial case'''
         grid_size = 0
         start = (0, 0, 0)
         goal = (0, 0, 0)
@@ -148,8 +149,9 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertIsNotNone(path)
                 self.assertEqual(path[0], start)
                 self.assertEqual(path[-1], goal)
-    
+
     def test_grid_3x3x3_3d(self):
+        '''Test small 3d grid'''
         grid_size = 3
         start = (0, 0, 0)
         goal = (2, 2, 2)
@@ -162,6 +164,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(path[-1], goal)
 
     def test_grid_1000x1000x1000_3d(self):
+        '''Test large 3d grid'''
         grid_size = 1000
         start = (0, 0, 0)
         goal = (49, 49, 29)
@@ -176,6 +179,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), expected_length)
 
     def test_no_possible_path_3d(self):
+        '''Test 3d graph without any possible path between start and goal'''
         grid_size = 10
         start = (0, 0, 0)
         goal = (10, 10, 10)  # Goal outside grid bounds
@@ -186,8 +190,9 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertIsNotNone(path)
                 self.assertEqual(path, [])
                 self.assertEqual(len(path), 0)
-    
+
     def test_start_equals_goal_3d(self):
+        '''Test when start coordinate is same as goal coordinate'''
         grid_size = 10
         start = (5, 5, 5)
         goal = (5, 5, 5)
@@ -201,6 +206,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), 1)
 
     def test_random_grids_3d(self):
+        '''Test randomized 3d grids'''
         for _ in range(25):
             grid_size = randint(1, 50)
             start = (randint(0, grid_size - 1), randint(0, grid_size - 1), randint(0, grid_size - 1))
@@ -216,6 +222,7 @@ class TestAStarSearch(unittest.TestCase):
                     self.assertEqual(len(path), expected_length)
 
     def test_edge_traversal_3d(self):
+        '''Test when optimal path travels up the edge'''
         grid_size = 5
         start = (0, 0, 0)
         goal = (0, 0, 4)  # Travel along edge only
@@ -230,6 +237,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), expected_length)
 
     def test_corner_to_corner_3d(self):
+        '''Test the furthest possible distance between start and goal'''
         grid_size = 10
         start = (0, 0, 0)
         goal = (9, 9, 9)  # From one corner to the opposite corner
@@ -244,6 +252,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), expected_length)
 
     def test_single_layer_grid_3d(self):
+        '''Test 3d grid when start and goal are on same level'''
         grid_size = 1
         start = (0, 0, 0)
         goal = (0, 0, 0)  # Only one possible node
@@ -255,6 +264,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), 1)
 
     def test_large_sparse_grid_3d(self):
+        '''Test large grid'''
         grid_size = 100
         start = (0, 0, 0)
         goal = (0, 0, 59)  # Straight line traversal along one dimension
@@ -269,6 +279,7 @@ class TestAStarSearch(unittest.TestCase):
                 self.assertEqual(len(path), expected_length)
 
     def test_random_start_goal_at_boundaries_3d(self):
+        '''Test randomized boundaries, start and goal'''
         grid_size = 50
         boundaries = [0, grid_size - 1]
 
